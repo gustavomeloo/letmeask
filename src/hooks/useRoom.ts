f3eee17096f69b909fb4e7606f0fd474496b8111
gu807 <gugamelo2@gmail.com>
2022-06-22 02:06:38 -0300

    adminRoom

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { database, ref, onValue } from '../services/firebase'
+
+type QuestionType = {
+  id: string;
+  author : {
+    name : string,
+    avatar : string
+  }
+
+  content : string,
+  isAnswered: string,
+  isHighlighted : string
+}
+
+type FirebaseQuestions = Record<string, {
+  author : {
+    name : string,
+    avatar : string
+  }
+
+  content : string,
+  isAnswered: string,
+  isHighlighted : string
+
+}>
+
+export function useRoom(roomId : string){
+  const [questions, setQuestions] = useState<QuestionType[]>([])
+  const [title, setTitle] = useState('')
+
+  useEffect(() => {
+    const roomRef = ref(database, `rooms/${roomId}`)
+
+    onValue(roomRef, room => {
+
+      const databaseRoom = room.val()
+
+      const FirebaseQuestions : FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(FirebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted : value.isHighlighted,
+          isAnswered : value.isAnswered,  
+        }
+      })
+
+      setTitle(databaseRoom.title)
+      setQuestions(parsedQuestions)
+    })
+
+  },[roomId])
+
+  return { questions, title }
+}
